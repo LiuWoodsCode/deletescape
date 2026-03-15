1add8df46e333980eaab9d8e8ada90ff59f1ee7e
Liu Woods <68132166+LiuWoodsCode@users.noreply.github.com>
2026-03-15 14:59:13 -0400

    Fix desktopshell dark mode on eben_b and other linux targets
    Fix issue where DesktopShell failed to be dark in the Fusion theming used on Linux, most notably Debian trixie on rpi_eben-b.

--- desktopshell.py
+++ desktopshell.py
@@ -351,6 +351,22 @@ class MdiShell(QMainWindow):
         app = QGuiApplication.instance()
 
         if self.config.dark_mode:
+            if not os.name == "nt":
+                palette = QPalette()
+                palette.setColor(QPalette.Window, QColor(53, 53, 53))
+                palette.setColor(QPalette.WindowText, Qt.white)
+                palette.setColor(QPalette.Base, QColor(35, 35, 35))
+                palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+                palette.setColor(QPalette.ToolTipBase, Qt.white)
+                palette.setColor(QPalette.ToolTipText, Qt.white)
+                palette.setColor(QPalette.Text, Qt.white)
+                palette.setColor(QPalette.Button, QColor(53, 53, 53))
+                palette.setColor(QPalette.ButtonText, Qt.white)
+                palette.setColor(QPalette.BrightText, Qt.red)
+                palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+                palette.setColor(QPalette.HighlightedText, Qt.black)
+
+                app.setPalette(palette)
             app.styleHints().setColorScheme(Qt.ColorScheme.Dark)
         else:
             app.styleHints().setColorScheme(Qt.ColorScheme.Light)

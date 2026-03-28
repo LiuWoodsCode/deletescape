249273ac7660a9a1b862c2464a343e9b69298245
Pixel Prowler <68132166+LiuWoodsCode@users.noreply.github.com>
2026-03-28 18:14:15 -0400

    Fix issues with KDE Plasma's dark mode

--- home.py
+++ home.py
@@ -1308,7 +1308,21 @@ class Deletescape(QMainWindow):
                 app.setPalette(palette)
 
             else:
+                palette = QPalette()
                 app.styleHints().setColorScheme(Qt.ColorScheme.Light)
+                palette.setColor(QPalette.Window, QColor("#F0F0F0"))
+                palette.setColor(QPalette.WindowText, Qt.black)
+                palette.setColor(QPalette.Base, Qt.white)
+                palette.setColor(QPalette.AlternateBase, QColor("#E0E0E0"))
+                palette.setColor(QPalette.ToolTipBase, Qt.black)
+                palette.setColor(QPalette.ToolTipText, Qt.white)
+                palette.setColor(QPalette.Text, Qt.black)
+                palette.setColor(QPalette.Button, QColor("#E0E0E0"))
+                palette.setColor(QPalette.ButtonText, Qt.black)
+                palette.setColor(QPalette.BrightText, Qt.red)
+                palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+                palette.setColor(QPalette.HighlightedText, Qt.white)
+                app.setPalette(palette)
         else:
             if self.config.dark_mode:
                 app.styleHints().setColorScheme(Qt.ColorScheme.Dark)

6da5505b3beb5203fc448e82ce7b66c0e371ac42
Pixel Prowler <68132166+LiuWoodsCode@users.noreply.github.com>
2026-06-08 17:35:28 -0400

    Add start of mobile ds mutiprocess shell, status bar

---
+++ labwc/mobile/status.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+import time
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("GtkLayerShell", "0.1")
+from gi.repository import GLib, Gtk, GtkLayerShell
+
+
+BAR_HEIGHT = 24
+
+
+class StatusBar(Gtk.Window):
+    def __init__(self) -> None:
+        super().__init__(title="deletescape mobile status")
+
+        self.set_decorated(False)
+        self.set_resizable(False)
+        self.set_default_size(1, BAR_HEIGHT)
+
+        GtkLayerShell.init_for_window(self)
+        GtkLayerShell.set_layer(self, GtkLayerShell.Layer.TOP)
+        GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.TOP, True)
+        GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.LEFT, True)
+        GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.RIGHT, True)
+        GtkLayerShell.set_exclusive_zone(self, BAR_HEIGHT)
+
+        self.clock = Gtk.Label()
+        self.clock.set_xalign(0.0)
+        self.clock.set_margin_start(8)
+        self.clock.set_margin_end(8)
+
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        box.set_size_request(-1, BAR_HEIGHT)
+        box.pack_start(self.clock, True, True, 0)
+        self.add(box)
+
+        self._update_clock()
+        GLib.timeout_add_seconds(1, self._update_clock)
+
+    def _update_clock(self) -> bool:
+        self.clock.set_text(time.strftime("%H:%M"))
+        return True
+
+
+def main() -> None:
+    bar = StatusBar()
+    bar.connect("destroy", Gtk.main_quit)
+    bar.show_all()
+    Gtk.main()
+
+
+if __name__ == "__main__":
+    main()
